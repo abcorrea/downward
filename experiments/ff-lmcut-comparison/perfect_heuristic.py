08a81f2a91313baac3321c1756b9162ab988c410
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2020-10-02 00:24:08 +0200

    Star branch to compare ff and lmcut

---
+++ experiments/ff-lmcut-comparison/perfect_heuristic.py
@@ -0,0 +1,29 @@
+class PerfectHeuristic(object):
+    """Compute the perfect heuristic from iPDB runs.
+
+    """
+
+    def __init__(self):
+        self.tasks_to_costs = defaultdict(lambda:None)
+
+    def _get_task(self, run):
+        return (run["domain"], run["problem"])
+
+    def _compute_difference(self, cost, h_star):
+        if cost is None or h_star is None:
+            return None
+        return abs(h_star - cost)
+
+    def store_costs(self, run):
+        if 'ipdb' in run["algorithm"]:
+            cost = run.get("cost")
+            if cost is not None:
+                assert run["coverage"]
+                self.tasks_to_costs[self._get_task(run)].append(cost)
+        return True
+
+    def add_diff_h_star(self, run):
+        run["diff_h_star"] = self._compute_difference(
+            run.get("cost"), self.tasks_to_costs[self._get_task(run)]
+        )
+        return run
---
+++ experiments/ff-lmcut-comparison/v1.py
@@ -0,0 +1,79 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import itertools
+import os
+
+from lab.environments import LocalEnvironment, BaselSlurmEnvironment
+from lab.reports import Attribute, geometric_mean
+
+from downward.reports.compare import ComparativeReport
+
+import common_setup
+from common_setup import IssueConfig, IssueExperiment
+
+from perfect_heuristic import PerfectHeuristic
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+SCRIPT_NAME = os.path.splitext(os.path.basename(__file__))[0]
+BENCHMARKS_DIR = os.environ["DOWNWARD_BENCHMARKS"]
+REVISIONS = ["ff-lmcut-comparison"]
+BUILDS = ["release"]
+CONFIG_NICKS = [
+    ('ipdb', ['--search', 'astar(ipdb(max_time=1200))']),
+    ('lmcut', ['--search', 'astar(lmcut)']),
+    ('ff', ['--search', 'astar(ff)']),
+    ('hmax', ['--search', 'astar(hmax)']),
+    ('add', ['--search', 'astar(add)']),
+]
+
+CONFIGS = [
+    IssueConfig(
+        config_nick,
+        config,
+        build_options=[build],
+        driver_options=["--build", build])
+    for build in BUILDS
+    for config_nick, config in CONFIG_NICKS
+]
+
+SUITE = common_setup.DEFAULT_OPTIMAL_SUITE
+ENVIRONMENT = BaselSlurmEnvironment(
+    partition="infai_2",
+    export=["PATH", "DOWNWARD_BENCHMARKS"])
+
+if common_setup.is_test_run():
+    SUITE = IssueExperiment.DEFAULT_TEST_SUITE
+    ENVIRONMENT = LocalEnvironment(processes=4)
+
+exp = IssueExperiment(
+    revisions=REVISIONS,
+    configs=CONFIGS,
+    environment=ENVIRONMENT,
+)
+exp.add_suite(BENCHMARKS_DIR, SUITE)
+
+exp.add_parser(exp.EXITCODE_PARSER)
+exp.add_parser(exp.TRANSLATOR_PARSER)
+exp.add_parser(exp.SINGLE_SEARCH_PARSER)
+exp.add_parser(exp.PLANNER_PARSER)
+
+exp.add_step('build', exp.build)
+exp.add_step('start', exp.start_runs)
+exp.add_fetcher(name='fetch')
+
+
+
+ATTRIBUTES = [
+    "coverage",
+    "initial_h_value",
+    "cost",
+    "diff_h_star"
+]
+#attributes = exp.DEFAULT_TABLE_ATTRIBUTES
+#attributes.extend(extra_attributes)
+
+obj = PerfectHeuristic()
+exp.add_absolute_report_step(attributes=ATTRIBUTES, filter=[obj.store_costs, obj.add_diff_h_star])
+
+exp.run_steps()
